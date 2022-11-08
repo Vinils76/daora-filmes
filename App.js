d97968e0c4f400c0022c2bbb04fc01ae973b6243
Vinils76 <antoniovinicius7670@gmail.com>
2022-11-08 09:47:29 -0300

    Componentes e estilização

--- App.js
+++ App.js
@@ -1,113 +1,18 @@
-import {
-  Image,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { useFonts } from "expo-font";
-import logo from "./assets//images/logo.png";
-import { Ionicons } from "@expo/vector-icons";
+import { StatusBar } from "react-native";
+import Favoritos from "./src/screen/Favoritos";
+import Home from "./src/screen/Home";
+import Privacidade from "./src/screen/Privacidade";
+import Sobre from "./src/screen/Sobre";
 
-const corPrimaria = "#5451a6";
+/* import Home from "./src/screen/Home"; */
 
 const App = () => {
-  const [fonteCarregada] = useFonts({
-    // instalar no terminal (npm install expo-font)
-    monoton: require("./assets/fonts/Monoton-Regular.ttf"),
-  });
-
-  if (!fonteCarregada) return <Text>Fonte sendo carregada...</Text>;
-
   return (
-    <SafeAreaView style={estilos.container}>
-      <View style={estilos.viewLogo}>
-        <Image style={estilos.logo} source={logo} />
-        <Text style={estilos.tituloApp}>Da hora filmes</Text>
-      </View>
-
-      <View style={estilos.viewBotoes}>
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="film-outline" size={16} color="white" /> Buscar
-            Filmes
-          </Text>
-        </Pressable>
-
-        <Pressable style={estilos.botaoInicial}>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="star" size={16} color="gold" /> Favoritos
-          </Text>
-        </Pressable>
-      </View>
-
-      <View style={estilos.viewRodape}>
-        <Pressable>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="lock-closed" size={16} color="white" /> Privacidade
-          </Text>
-        </Pressable>
-
-        <Pressable>
-          <Text style={estilos.textoBotao}>
-            <Ionicons name="information-circle" size={16} color="white" /> Sobre
-          </Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
+    <>
+      <StatusBar />
+      <Sobre />
+    </>
   );
 };
 
 export default App;
-
-const estilos = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  viewLogo: {
-    flex: 3,
-    /* width: "80%", */
-    textAlign: "center",
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  /* logo: {
-    width: 128,
-    height: 128,
-  }, */
-  tituloApp: {
-    fontSize: 36,
-    fontFamily: "monoton",
-    color: corPrimaria,
-    //fontWeight: "bold",
-  },
-  viewBotoes: {
-    flex: 2, // Espaço ocupado do
-    flexDirection: "row",
-    justifyContent: "space-evenly",
-    alignItems: "flex-start",
-    width: "80%",
-  },
-  botaoInicial: {
-    borderStyle: "solid",
-    borderWidth: 2,
-    padding: 16,
-    backgroundColor: corPrimaria,
-  },
-  textoBotao: {
-    color: "white",
-  },
-  viewRodape: {
-    flex: 0.5,
-    backgroundColor: corPrimaria,
-    flexDirection: "row", //deixa lado a lado
-    justifyContent: "space-between", // dá espaço entre o conteúdo
-    alignItems: "center",
-    width: "100%",
-    paddingHorizontal: 16,
-  },
-});
---
+++ src/screen/Home.js
@@ -0,0 +1,111 @@
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { useFonts } from "expo-font";
+import logo from "../../assets/images/logo.png";
+import { Ionicons } from "@expo/vector-icons";
+
+const Home = () => {
+  const [fonteCarregada] = useFonts({
+    // instalar no terminal (npm install expo-font)
+    monoton: require("../../assets/fonts/Monoton-Regular.ttf"),
+  });
+
+  if (!fonteCarregada) return <Text>Fonte sendo carregada...</Text>;
+  return (
+    <SafeAreaView style={estilos.container}>
+      <View style={estilos.viewLogo}>
+        <Image style={estilos.logo} source={logo} />
+        <Text style={estilos.tituloApp}>Da hora filmes</Text>
+      </View>
+
+      <View style={estilos.viewBotoes}>
+        <Pressable style={estilos.botaoInicial}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="film-outline" size={16} color="white" /> Buscar
+            Filmes
+          </Text>
+        </Pressable>
+
+        <Pressable style={estilos.botaoInicial}>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="star" size={16} color="gold" /> Favoritos
+          </Text>
+        </Pressable>
+      </View>
+
+      <View style={estilos.viewRodape}>
+        <Pressable>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="lock-closed" size={16} color="white" /> Privacidade
+          </Text>
+        </Pressable>
+
+        <Pressable>
+          <Text style={estilos.textoBotao}>
+            <Ionicons name="information-circle" size={16} color="white" /> Sobre
+          </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+/* const corPrimaria = "#5451a6"; */
+
+const estilos = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  viewLogo: {
+    flex: 3,
+    /* width: "80%", */
+    textAlign: "center",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  /* logo: {
+      width: 128,
+      height: 128,
+    }, */
+  tituloApp: {
+    fontSize: 36,
+    fontFamily: "monoton",
+    color: "#5451a6",
+    //fontWeight: "bold",
+  },
+  viewBotoes: {
+    flex: 2, // Espaço ocupado do
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+    width: "80%",
+  },
+  botaoInicial: {
+    borderStyle: "solid",
+    borderWidth: 2,
+    padding: 16,
+    backgroundColor: "#5451a6",
+  },
+  textoBotao: {
+    color: "white",
+  },
+  viewRodape: {
+    flex: 0.5,
+    backgroundColor: "#5451a6",
+    flexDirection: "row", //deixa lado a lado
+    justifyContent: "space-between", // dá espaço entre o conteúdo
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 16,
+  },
+});

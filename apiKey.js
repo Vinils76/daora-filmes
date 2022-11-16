c39d9d2a346a527cb11f30366ecd5c8cfd53f918
Vinils76 <antoniovinicius7670@gmail.com>
2022-11-16 09:05:27 -0300

    Melhoria de busca

---
+++ apiKey.js
@@ -0,0 +1,3 @@
+const api = "c5f93549d80ddef4ace7c13ae4cd711d";
+
+export default api;
--- src/screen/Resultados.js
+++ src/screen/Resultados.js
@@ -1,29 +1,35 @@
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import api from "../services/api";
+import apiKey from "../../apiKey";
+import Loading from "../components/Loading";
 
 const Resultados = ({ route }) => {
-  /* Usamos a prop route (do React Navigation) para
-    acessar os parâmetros desta rota de navegação e extrair
-    os dados (neste caso, filme) enviados para esta tela Resultados */
   const { filme } = route.params;
 
   const [resultados, setResultados] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function buscarFilmes() {
       try {
         const resposta = await api.get("/search/movie", {
           params: {
-            api_key: "c5f93549d80ddef4ace7c13ae4cd711d",
-            language: "pt-br",
+            api_key: apiKey,
+            language: "pt-BR",
             query: filme,
             include_adult: false,
           },
         });
 
         setResultados(resposta.data.results);
+
+        /* Simulando um tempo de carregamento lento
+        usando temporizador setInterval */
+        setInterval(() => {
+          setLoading(false);
+        }, 3000);
       } catch (error) {
         console.log("Deu ruim na busca na API: " + error.message);
       }
@@ -31,15 +37,31 @@ const Resultados = ({ route }) => {
     buscarFilmes();
   }, []);
 
-  console.log(resultados);
-
   return (
     <SafeAreaView style={estilos.container}>
       <Text>Você buscou por: {filme} </Text>
+
+      {/* Sintaxe de if evaluate usando &&
+Se loading for TRUE, renderize <Loading /> */}
+      {loading && <Loading />}
+
       <View style={estilos.viewFilmes}>
-        {resultados.map((resultado) => {
-          return <Text key={resultado.id}> {resultado.title}</Text>;
-        })}
+        {/* Se loading for false, renderize o resultado
+        do map */}
+        {!loading &&
+          resultados.map((resultado) => {
+            return (
+              <View key={resultado.id}>
+                <Image
+                  style={estilos.imagem}
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/original/${resultado.poster_path}`,
+                  }}
+                />
+                <Text> {resultado.title} </Text>
+              </View>
+            );
+          })}
       </View>
     </SafeAreaView>
   );
@@ -48,11 +70,14 @@ const Resultados = ({ route }) => {
 export default Resultados;
 
 const estilos = StyleSheet.create({
+  viewFilmes: {
+    marginVertical: 8,
+  },
   container: {
     flex: 1,
     padding: 16,
   },
-  viewFilmes: {
-    marginVertical: 8,
+  imagem: {
+    height: 125,
   },
 });
